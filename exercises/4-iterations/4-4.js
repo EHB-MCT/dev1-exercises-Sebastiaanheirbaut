600ae271ef5fca482f028851456b879a64ef4f2a
PSebastiaan Heirbaut <sebastiaan.heirbaut@student.ehb.be>
2022-10-27 12:21:02 +0200

    Finish excersise 4-iterations

--- exercises/4-iterations/4-4.js
+++ exercises/4-iterations/4-4.js
@@ -2,21 +2,70 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-let width = context.canvas.width;
-let height = context.canvas.height;
-let radius = 50;
+drawCircles6();
 
-drawRandomCircles();
-
-function drawRandomCircles() {
-    for(let i=0; i<10; i++){
-        Utils.fillCircle(i*105, 0, radius);
+function drawCircles1() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsl(200, 70, 70);
+            Utils.fillCircle(x, y, 50);
+        }
     }
+}
 
-    for(let i=0; i<10; i++){
-        Utils.fillCircle(i*105, i* 10, radius);
+function drawCircles2() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(200, 70, 70, 50);
+            Utils.fillCircle(x, y, Math.random() * 100);
+        }
     }
+}
 
-    
-   
+function drawCircles3() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(200, 70, 70, 50);
+            Utils.fillCircle(x, y, 20 + y / 10);
+        }
+    }
+}
+
+function drawCircles4() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsl(i % 2 * 200, 70, 70);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
+}
+
+function drawCircles5() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsl(j % 2 * 200, 70, 70);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
+}
+
+function drawCircles6() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsl((i + j) % 2 * 200, 70, 70);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
 }
